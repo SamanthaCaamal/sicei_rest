ff25aa01c3f6ab7ab485b4a986f0c673777e72a7
SamanthaCaamal <40219086+SamanthaCaamal@users.noreply.github.com>
2021-09-30 13:22:17 -0500

    Eliminar array innecesario en test

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,13 +2,6 @@ const supertest = require("supertest")
 const app = require("../src/index")
 const api = supertest(app)
 
-const students = [
-    {
-      name: 'Samantha Caamal',
-      matricula: 'a13008207'
-    }
-  ]
-
 test('GET students are returned as json', () =>{
     api
     .get('/api/students')
